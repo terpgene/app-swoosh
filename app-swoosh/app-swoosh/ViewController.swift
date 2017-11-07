fa4a601412396d3f6ce84dc81303d032acff8c47
Gene K. Essel <eugene2essel@gmail.com>
2017-11-07 18:04:20 -0500

    Added segue and back button

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -18,10 +18,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
     }
 
 
