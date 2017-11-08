c62f2ff39ab632b5ca95a266c7538b8eca86bb84
Gene K. Essel <eugene2essel@gmail.com>
2017-11-08 00:24:39 -0500

    Added structs to pass data btn VC

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,27 +9,42 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        player = Player()
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+        
     }
-    */
-
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String){
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
+    
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Gene Essel on 11/7/17.
+//  Copyright © 2017 Terpgene. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
